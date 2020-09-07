f449811dcb72f7513db923f52a84da037410014d
mahmoud.shawky <mahmoud.shawky25@gmail.com>
2020-09-07 04:10:03 +0200

    add screenshots

--- app/src/main/java/eg/mahmoudShawky/metar/utils/Constants.java
+++ app/src/main/java/eg/mahmoudShawky/metar/utils/Constants.java
@@ -7,6 +7,6 @@ package eg.mahmoudShawky.metar.utils;
 public class Constants {
     public static final String DB_NAME = "metar_database";
     public static final String GERMANY_STATIONS_PREFIX = "ED";
-    public static final int UPDATE_INTERVAL_MIN = 15;
-    public static final int UPDATE_INTERVAL_MAX = 30;
+    public static final int UPDATE_INTERVAL_MIN = 60;
+    public static final int UPDATE_INTERVAL_MAX = 90;
 }
